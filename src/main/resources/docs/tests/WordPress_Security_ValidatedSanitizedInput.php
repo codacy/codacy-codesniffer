e57d8da3660d9d251f4bc9b218987f1ad8f82be2
Daniel Reigada <daniel@codacy.com>
2018-10-15 12:05:56 +0100

    Add test with list parameters

---
+++ src/main/resources/docs/tests/WordPress_Security_ValidatedSanitizedInput.php
@@ -0,0 +1,17 @@
+<?php
+
+//#Patterns: WordPress_Security_ValidatedSanitizedInput : { "customSanitizingFunctions" : "do_something_with, bar_sanitized"}
+
+do_something( $_POST ); // Ok.
+
+//#Err: WordPress_Security_ValidatedSanitizedInput
+do_something_with( $_POST['hello'] ); // Error for no validation, Error for no sanitizing, Error for no unslashing.
+
+//#Err: WordPress_Security_ValidatedSanitizedInput
+echo sanitize_text_field( wp_unslash( $_POST['foo1'] ) ); // Error for no validation.
+
+if ( isset( $_POST['foo2'] ) ) {
+	bar_sanitized( wp_unslash( $_POST['foo2'] ) ); // No Error (customSanitizingFunctions)
+	//#Err: WordPress_Security_ValidatedSanitizedInput
+	bar_not_sanitized( wp_unslash( $_POST['foo2'] ) ); // Error for no sanitizing.
+}
