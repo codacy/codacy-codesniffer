e8273aa2c58b02f6625470d5ebed9ea5524e534f
Luís Ferreira <contact@lsferreira.net>
2019-10-18 17:09:48 +0100

    tests: update tests according to the new tool version and plugins
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/main/resources/docs/tests/WordPress_Security_ValidatedSanitizedInput.php
+++ src/main/resources/docs/tests/WordPress_Security_ValidatedSanitizedInput.php
@@ -6,12 +6,14 @@ do_something( $_POST ); // Ok.
 
 //#Err: WordPress_Security_ValidatedSanitizedInput
 //#Err: WordPress_Security_ValidatedSanitizedInput
+//#Err: WordPress_Security_ValidatedSanitizedInput
 do_something_with( $_POST['hello'] ); // Error for no validation, Error for no unslashing.
 
 //#Err: WordPress_Security_ValidatedSanitizedInput
 echo sanitize_text_field( wp_unslash( $_POST['foo1'] ) ); // Error for no validation.
 
 if ( isset( $_POST['foo2'] ) ) {
+	//#Err: WordPress_Security_ValidatedSanitizedInput
 	bar_sanitized( wp_unslash( $_POST['foo2'] ) ); // No Error (customSanitizingFunctions)
 	//#Err: WordPress_Security_ValidatedSanitizedInput
 	bar_not_sanitized( wp_unslash( $_POST['foo2'] ) ); // Error for no sanitizing.
