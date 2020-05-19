398327c551b04436fe12cd96d2648000a113dab3
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-05-19 13:12:36 +0100

    test: Add VIP Wordpress multiple tests

---
+++ src/main/resources/docs/multiple-tests/vip-wordpress/src/User.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+namespace Test;
+
+class User
+{
+    public function setUsernameAttribute($value)
+    {
+        $notUsed = 1;
+        $this->attributes['username'] = "123username";
+        str_replace("username", "name", "");
+    }
+}
