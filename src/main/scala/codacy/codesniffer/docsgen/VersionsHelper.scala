840cb47ca947db94026ad12d5d0451101811b100
Vítor Pires <vitor.pires@codacy.com>
2024-05-03 05:04:11 +0100

    test with all standards

--- src/main/scala/codacy/codesniffer/docsgen/VersionsHelper.scala
+++ src/main/scala/codacy/codesniffer/docsgen/VersionsHelper.scala
@@ -10,15 +10,15 @@ object VersionsHelper {
     composerJson("require")
   }
 
-  lazy val codesniffer = properties("squizlabs/php_codesniffer").str
-  lazy val magentoEQP = properties("magento/marketplace-eqp").str
+  lazy val vipWordpress = properties("automattic/vipwpcs").str
+  lazy val doctrine = properties("doctrine/coding-standard").str
+  lazy val drupalCoder = properties("drupal/coder").str
+  lazy val symfony = properties("escapestudios/symfony2-coding-standard").str
   lazy val magentoCS = properties("magento/magento-coding-standard").str
-  lazy val wordpress = properties("wp-coding-standards/wpcs").str
+  lazy val magentoEQP = properties("magento/marketplace-eqp").str
   lazy val phpCompatibility = properties("phpcompatibility/php-compatibility").str
   lazy val phpcsSecurityAudit = properties("pheromone/phpcs-security-audit").str
   lazy val slevomatCS = properties("slevomat/coding-standard").str
-  lazy val drupalCoder = properties("drupal/coder").str
-  lazy val vipWordpress = properties("automattic/vipwpcs").str
-  lazy val symfony = properties("escapestudios/symfony2-coding-standard").str
-
+  lazy val codesniffer = properties("squizlabs/php_codesniffer").str
+  lazy val wordpress = properties("wp-coding-standards/wpcs").str
 }
